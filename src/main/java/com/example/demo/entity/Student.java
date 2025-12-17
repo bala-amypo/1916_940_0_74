b38c6a183fe9f7f4eaac5924492a43558d29f37f
bala-amypo <bala@amypo.in>
2025-12-17 05:21:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -25,34 +25,15 @@ public class Student {
     public void setEmail(String email) {
         this.email = email;
     }
-    public LocalDate getDob() {
-        return dob;
-    }
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-    public float getCgpa() {
-        return cgpa;
-    }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
-    }
-    public Student(int id, String name, String email, LocalDate dob, float cgpa) {
+    
+    public Student(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.dob = dob;
-        this.cgpa = cgpa;
+       
     }
     public Student() {
     }
 
-    // public void setId(int id) { //Setting the data Id
-    //     this.id = id;
-    // }
-    // public int getId() { //Getting the data Id
-    //     return id;
-    // }
-
-    
+   
 }
