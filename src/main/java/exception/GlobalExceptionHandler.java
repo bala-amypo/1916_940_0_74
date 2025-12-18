cb383b81a3788b59663bc3c30d339b4a8abe25bc
bala-amypo <bala@amypo.in>
2025-12-18 08:35:26 +0000

    User commit

--- src/main/java/exception/GlobalExceptionHandler.java
+++ src/main/java/exception/GlobalExceptionHandler.java
@@ -1,1 +1,31 @@
-package com.example.demo.exception; import org.springframework.web.bind.annotation.RestControllerAdvice; import org.springframework.web.bind.annotation.ExceptionHandler; import org.springframework.http.ResponseEntity; import java.util.Map; import org.springframework.http.HttpStatus; import org.springframework.web.bind.MethodArgumentNotValidException; @RestControllerAdvice public class GlobalExceptionHandler{ @ExceptionHandler(ResourceNotFoundException.class) //runtime expection public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex){ return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND); } @ExceptionHandler(MethodArgumentNotValidException.class) public ResponseEntity<Map<String,String>> handleMethod(MethodArgumentNotValidException mex){ Map<String,String> error=new HashMap<>(); ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()) ); return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST); } }
+package com.example.demo.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethod(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+          .getFieldErrors()
+          .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
